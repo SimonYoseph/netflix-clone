ade302a25fbb2f6184cc83cbd04f6738d91c8387
Simon Yoseph <syosep1@students.towson.edu>
2025-02-01 23:52:30 -0500

    updated auth.tsx components

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,18 @@
-import { useState } from "react"; 
+import { useCallback, useState } from "react"; 
 import Input from "@/components/Input";
 
 const Auth = () => {
     const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
+const [variant, setVariant] = useState('login');
+
+const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) => currentVariant == 'login' ? 'register' : 'login');
+
+}, [])
+
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-cover bg-center bg-no-repeat">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -15,15 +25,39 @@ const Auth = () => {
                         Sign In
                     </h2>
                     <div className= "flex flex-col gap-4">
-                        <Input 
-                        label="Email"
-                        onChange={() => {}}
-                        id="email"
-                        type="email"
-                        value=""
+                    <Input 
+                            label="Username"
+                            onChange={(ev: any) => setName(ev.target.value)}
+                            id="name"
+                            value={name}
                         />
 
-                    </div>
+                    <Input 
+                            label="Email"
+                            onChange={(ev: any) => setEmail(ev.target.value)}
+                            id="email"
+                            type="email"
+                            value={email}
+                        />
+
+                    <Input 
+                            label="Password"
+                            onChange={(ev: any) => setPassword(ev.target.value)}
+                            id="password"
+                            value={password}
+                        />
+                        </div>
+                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                        Login
+                        </button>
+                        <p className="text-neutral-500 mt-12">
+                            First time using Netflix?
+                            <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+                                Create an account
+
+                            </span>
+
+                        </p>
                     </div>
                 </div>
             </div>
