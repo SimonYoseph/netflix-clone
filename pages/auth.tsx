beb7b5f019053f74fd16c0a705709f0e06b5b4cc
Simon Yoseph <syosep1@students.towson.edu>
2025-02-01 22:27:05 -0500

    Intial commit

---
+++ pages/auth.tsx
@@ -0,0 +1,19 @@
+const Auth = () => {
+    return (
+        <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-cover bg-center bg-no-repeat">
+            <div className="bg-black w-full h-full lg:bg-opacity-50">
+                <nav className="px-12 py-5">
+                    <img src= "/images/logo.png" alt="Logo" className="h-12"/>
+                </nav>
+                <div className="flex justify-center">
+                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                    
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Auth;
